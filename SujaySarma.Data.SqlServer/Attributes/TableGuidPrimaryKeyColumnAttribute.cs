71583a91dba87e6b54816ed8fcce39c1edcdb0fc
sujayvsarma <anon@email.org>
2025-10-01 13:07:25 +0530

    (SQL) Added 2 new attributes `TableDateTimeAuditColumn` and `TableGuidPrimaryKey` equivalent to the base `DateTimeAuditColumn` and `GuidPrimaryKey` attributes, but these SQL attributes derive from `TableColummn` attribute and not from the base `ContainerMember` attribute!

---
+++ SujaySarma.Data.SqlServer/Attributes/TableGuidPrimaryKeyColumnAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SujaySarma.Data.SqlServer.Attributes
+{
+    /// <summary>
+    /// Provides an auto-gen value enabled primary key that operates on Guid values
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = false, Inherited = true)]
+    public class TableGuidPrimaryKeyColumnAttribute : TableColumnAttribute
+    {
+
+        /// <summary>
+        /// Provides an auto-gen value enabled primary key that operates on Guid values
+        /// </summary>
+        /// <param name="columnName">The name of the underlying table column</param>
+        public TableGuidPrimaryKeyColumnAttribute(string columnName)
+            : base(columnName)
+        {
+        }
+
+    }
+
+
+}
